efc7643273a48275dda87bfa1c397fc7bf9eceee
He Zhang (Rudy) <425220727@qq.com>
2016-12-28 16:41:43 +0800

    Update 008_StringToInteger.cpp

--- Cpp/008_StringToInteger.cpp
+++ Cpp/008_StringToInteger.cpp
@@ -1,3 +1,35 @@
+//#include <iostream>
+//#include <string>
+
+//using namespace std;
+
+
+//class Solution {
+//public:
+//	int myAtoi(string str) {
+//		int sign = 1, base = 0, i = 0;
+//		while (str[i] == ' ') { i++; }
+//		if (str[i] == '-' || str[i] == '+') {
+//			sign = 1 - 2 * (str[i++] == '-'); 
+//		}
+//		while (str[i] >= '0' && str[i] <= '9') {
+//			if (base >  INT_MAX / 10 || (base == INT_MAX / 10 && str[i] - '0' > 7)) { //  INT_MAX = 2147483647 最后一位是7
+//				if (sign == 1) return INT_MAX;
+//				else return INT_MIN;
+//			}
+//			base  = 10 * base + (str[i++] - '0');
+//		}
+//		return base * sign;
+//	}
+//};
+
+
+//void main() {
+//	Solution s;	
+//string str = "-2147483649";	
+//cout << s.myAtoi(str) << endl;
+	//cout << INT_MAX;
+//}
 #include <iostream>
 #include <string>
 
@@ -7,19 +39,21 @@ using namespace std;
 class Solution {
 public:
 	int myAtoi(string str) {
-		int sign = 1, base = 0, i = 0;
-		while (str[i] == ' ') { i++; }
-		if (str[i] == '-' || str[i] == '+') {
-			sign = 1 - 2 * (str[i++] == '-'); 
-		}
-		while (str[i] >= '0' && str[i] <= '9') {
-			if (base >  INT_MAX / 10 || (base == INT_MAX / 10 && str[i] - '0' > 7)) { //  INT_MAX = 2147483647 最后一位是7
-				if (sign == 1) return INT_MAX;
-				else return INT_MIN;
+		long long result = 0;
+		int indicator = 1;
+		for(int i = 0; i<str.size();)
+		{
+			i = str.find_first_not_of(' ');
+			if(str[i] == '-' || str[i] == '+')
+				indicator = (str[i++] == '-')? -1 : 1;
+			while('0'<= str[i] && str[i] <= '9') 
+			{
+				result = result*10 + (str[i++]-'0');
+				if(result*indicator >= INT_MAX) return INT_MAX;
+				if(result*indicator <= INT_MIN) return INT_MIN;                
 			}
-			base  = 10 * base + (str[i++] - '0');
+			return result*indicator;
 		}
-		return base * sign;
 	}
 };
 
@@ -29,4 +63,5 @@ void main() {
 	string str = "-2147483649";
 	cout << s.myAtoi(str) << endl;
 	//cout << INT_MAX;
+	//cout << str[0] << endl;
 }
