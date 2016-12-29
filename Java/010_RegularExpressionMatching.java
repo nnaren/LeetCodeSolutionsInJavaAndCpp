2a3dfa8afbc1081779d07e59e123e43f8a6a9ba0
He Zhang (Rudy) <425220727@qq.com>
2016-12-29 15:48:56 +0800

    Create 010_RegularExpressionMatching.java

---
+++ Java/010_RegularExpressionMatching.java
@@ -0,0 +1,41 @@
+package leetCode;
+
+public class regularMatcher {
+
+	public boolean isMatch(String s, String p) {
+        if(s == null || p == null) {
+        	return false;
+        }
+        
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int j = 0; j < p.length(); j++) {
+        	if (p.charAt(j) == '*' && dp[0][j-1]) {
+        		dp[0][j+1] = true;
+        	}
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+        	for (int j = 0; j < p.length(); j++) {
+        		if (s.charAt(i) == p.charAt(j)) {
+        			dp[i+1][j+1] = dp[i][j];
+        		}
+        		
+        		if (p.charAt(j) == '.') {
+        			dp[i+1][j+1] = dp[i][j];
+        		}
+        		
+        		if (p.charAt(j) == '*') {
+        			if (p.charAt(j-1) != s.charAt(i) && p.charAt(j-1) != '.') {
+        				dp[i+1][j+1] = dp[i+1][j-1];
+        			} else {
+        				dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
+        			}
+        		}
+        	}
+        }
+        return dp[s.length()][p.length()];
+	}
+}
+
+
