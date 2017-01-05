254c6b6252858b3f025a2a195e00be43e5e1ecba
He Zhang (Rudy) <425220727@qq.com>
2017-01-05 14:23:09 +0800

    Create 021_MergeTwoSortedLists.java

---
+++ Java/021_MergeTwoSortedLists.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    // public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    //     if(l1 == null) return l2;
+    //     if(l2 == null) return l1;
+        
+    //     ListNode res = new ListNode(0);
+    //     ListNode resTemp = res;
+    //     while(l1 != null || l2 != null) {
+    //     	if(l1 == null) {
+    //     		resTemp.next = l2;
+    //     		resTemp = resTemp.next;
+    //     		l2 = l2.next;
+    //     	} else if(l2 == null) {
+    //     		resTemp.next = l1;
+    //     		resTemp = resTemp.next;
+    //     		l1 = l1.next;
+    //     	} else if(l1.val < l2.val) {
+    //     		resTemp.next = l1;
+    //     		resTemp = resTemp.next;
+    //     		l1 = l1.next;
+    //     	} else {
+    //     		resTemp.next = l2;
+    //     		resTemp = resTemp.next;
+    //     		l2 = l2.next;
+    //     	}
+    //     }
+        
+    //     resTemp.next = null;
+    //     return res.next;
+    // }
+    
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		if(l1 == null) return l2;
+		if(l2 == null) return l1;
+		
+		if(l1.val < l2.val) {
+			l1.next = mergeTwoLists(l1.next, l2);
+			return l1;
+		} else {
+			l2.next = mergeTwoLists(l1, l2.next);
+			return l2;
+		}
+	}
+}
