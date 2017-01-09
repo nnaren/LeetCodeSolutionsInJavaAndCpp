e5ed758892e1150e131e8785c94506b69cd8d811
He Zhang (Rudy) <425220727@qq.com>
2017-01-09 17:35:06 +0800

    Update 028_ImplementStrStr.java

--- Java/028_ImplementStrStr.java
+++ Java/028_ImplementStrStr.java
@@ -52,3 +52,65 @@ public class Solution {
 		return i;
 	}
 }
+
+////KMP Algorithm
+// package leetCode;
+
+// public class KMPSearch {
+	
+// 	public void kmpSearch(String txt, String pat) {
+// 		int M = pat.length();
+// 		int N = txt.length();
+// 		int j = 0;
+// 		int[] lps = new int[M];
+// 		computeLPSArray(pat, M, lps);
+// 		int i = 0;
+// 		while (i < N) {
+// 			if (pat.charAt(j) == txt.charAt(i)) {
+// 				i++;
+// 				j++;
+// 			}
+// 			if(j == M) {
+// 				System.out.println("index:" + (i-j));
+// 				j = lps[j-1];
+// 			} else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+// 				if (j != 0)
+// 					j = lps[j-1];
+// 				else 
+// 					i = i+1;
+// 			}
+// 		}
+// 	}
+	
+// 	void computeLPSArray(String pat, int M, int[] lps) {
+// 		// length of the previous longest prefix suffix
+// 		int len = 0;
+// 		int i = 1;
+// 		lps[0] = 0;  // lps[0] is always 0
+		
+// 		// the loop calculates lps[i] for i = 1 to M-1
+// 		while (i < M) {
+// 			if (pat.charAt(i) == pat.charAt(len)) {
+// 				len++;
+// 				lps[i] = len;
+// 				i++;
+// 			} else { // (pat[i] != pat[len])
+// 				// This is tricky. Consider the example.
+//                 // AAACAAAA and i = 7. The idea is similar 
+//                 // to search step.
+// 				if (len != 0) {
+// 					len = lps[len-1];
+// 				} else {
+// 					lps[i] = len;
+// 					i++;
+// 				}
+// 			}
+// 		}
+// 	}
+	
+// 	public static void main(String[] args) {
+// 		// TODO Auto-generated method stub
+
+// 	}
+
+// }
