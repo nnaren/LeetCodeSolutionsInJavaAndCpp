88ecb919dcd1179ede389e2865891f67eb9e5655
He Zhang (Rudy) <425220727@qq.com>
2017-01-13 15:09:30 +0800

    Update 034_SearchForARange.java

--- Java/034_SearchForARange.java
+++ Java/034_SearchForARange.java
@@ -53,3 +53,30 @@ public class Solution {
 		} else return -1;
 	}
 }
+
+
+
+///method 2
+public class Solution {
+    public int[] searchRange(int[] nums, int target) {
+		int[] res = {-1, -1};
+		if(nums.length == 0) return res;
+		int i = 0, j = nums.length-1;
+		while (i < j) {
+			int mid = (i + j) / 2;
+			if (nums[mid] < target) i = mid + 1;
+			else j = mid;
+		}
+		if (nums[i] != target) return res;
+		else res[0] = i;
+		
+		j = nums.length - 1;
+		while (i < j) {
+			int mid = (i + j) / 2 + 1;
+			if (nums[mid] > target) j = mid - 1;
+			else i = mid;
+		}
+		res[1] = j;
+		return res;
+	}
+}
