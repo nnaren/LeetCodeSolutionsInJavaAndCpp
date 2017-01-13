6f97488a4033d6452df4295182f284bede1ad81e
He Zhang (Rudy) <425220727@qq.com>
2017-01-13 17:11:47 +0800

    Update 036_ValidSudoku.java

--- Java/036_ValidSudoku.java
+++ Java/036_ValidSudoku.java
@@ -15,3 +15,25 @@ public class Solution {
         return true;
     }
 }
+
+//method 2
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int[][] row = new int[9][9];
+        int[][] col = new int[9][9];
+        int[][] block = new int[9][9];
+        
+        for(int i = 0; i < 9; i++) {
+        	for(int j = 0; j < 9; j++) {
+        		if(board[i][j] != '.') {
+        			int num = board[i][j] - '0' - 1;
+        			int k = i / 3 * 3 + j / 3;
+        			if(row[i][num] == 1 || col[j][num] == 1 || block[k][num] == 1)
+        				return false;
+        			row[i][num] = col[j][num] = block[k][num] = 1;
+        		}
+        	}
+        }
+        return true;
+	}
+}
