4447206a57685d40bbd68119fd65a151860fc694
He Zhang (Rudy) <425220727@qq.com>
2017-01-14 21:20:43 +0800

    Update 037_SudokuSolver.cpp

--- Cpp/037_SudokuSolver.cpp
+++ Cpp/037_SudokuSolver.cpp
@@ -38,3 +38,44 @@ public:
         return true;
     }
 };
+
+
+
+//
+class Solution {
+public:
+	
+	void solveSudoku(vector<vector<char> > &board) {
+		solve(board, 0);
+	}
+
+	bool solve(vector<vector<char> >& board, int pos) {
+		if(pos >= 81) return true;
+		int i = pos / 9;
+		int j = pos % 9;
+		if(board[i][j] != '.')
+			return solve(board, pos+1);
+		else {
+			for(char c = '1'; c <= '9'; c++) {
+				if(isValid(board, i, j, c)) {
+					board[i][j] = c;
+					if(solve(board, pos+1))
+						return true;
+					else {
+						board[i][j] = '.';
+					}
+				}
+			}
+			return false;
+		}
+	}
+
+	bool isValid(vector<vector<char> >& board, int row, int col, char c) {
+		for(int k = 0; k < 9; k++) {
+			if(board[row][k] == c) return false;
+			if(board[k][col] == c) return false;
+			if(board[row/3*3+k/3][col/3*3+k%3] == c) return false;
+		}
+		return true;
+	}
+};
