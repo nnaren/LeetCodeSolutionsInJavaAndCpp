8f2b65013fbce43a3ad7d8d1fb4a43ee5d205da4
He Zhang (Rudy) <425220727@qq.com>
2017-01-25 23:54:33 +0800

    Create 041_FirstMissingPositive.java

---
+++ Java/041_FirstMissingPositive.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int firstMissingPositive(int[] nums) {
+		int n = nums.length;
+		for(int i = 0; i < n; ++i) {
+			while(nums[i] > 0 && nums[i] <= n && nums[nums[i]-1] != nums[i]) {
+				swap(i, nums[i]-1, nums);
+			}
+		}
+		for(int i = 0; i < n; ++i) {
+			if(nums[i] != i + 1)
+				return i+1;
+		}
+		return n+1;
+    }
+	
+	private void swap(int a, int b, int[] nums) {
+		nums[a] = nums[a] ^ nums[b];
+		nums[b] = nums[a] ^ nums[b];
+		nums[a] = nums[a] ^ nums[b];
+	}
+}
