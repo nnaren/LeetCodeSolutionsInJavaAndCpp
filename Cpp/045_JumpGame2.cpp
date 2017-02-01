95897926cbb81953d507ab23ea0edebbeda09e04
He Zhang (Rudy) <425220727@qq.com>
2017-02-01 15:37:12 +0800

    Update 045_JumpGame2.cpp

--- Cpp/045_JumpGame2.cpp
+++ Cpp/045_JumpGame2.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int curEnd = 0, curFarthest = 0, jumps = 0;
+        for(int i = 0; i < nums.size()-1; i++) {
+            curFarthest = max(curFarthest, i+nums[i]);
+            if(i == curEnd) {
+                jumps++;
+                curEnd = curFarthest;
+            }
+        }
+        return jumps;
+    }
+};
