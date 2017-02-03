0b491d9374ec2dd6261ad0dea16b60d888e9e90f
He Zhang (Rudy) <425220727@qq.com>
2017-02-03 16:08:32 +0800

    Update 050_Pow(x, n).java

--- Java/050_Pow(x, n).java
+++ Java/050_Pow(x, n).java
@@ -5,3 +5,18 @@ public class Solution {
         return n % 2 == 0 ? res*res : (n < 0 ? res*res*(1/x) : res*res*x);
     }
 }
+
+
+//method 2
+public class Solution {
+    public double myPow(double x, int n) {
+		if(n == Integer.MIN_VALUE && x > 1)
+			return 0;
+		if(n == 0) return 1;
+		if(n < 0) {
+			n = -n;
+			x = 1/x;
+		}
+		return (n%2 == 0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
+	}
+}
