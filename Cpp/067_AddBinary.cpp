60cceba8397d0f429905eba9762ec6da95a7ac0b
He Zhang (Rudy) <425220727@qq.com>
2017-02-03 17:09:06 +0800

    Create 067_AddBinary.cpp

---
+++ Cpp/067_AddBinary.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+		string res;
+		int flag = 0;
+		int lenA = a.length()-1, lenB = b.length()-1;
+		int key = 0;
+		while (lenA >= 0 || lenB >= 0) {
+			if(lenA < 0) {
+				key = flag + (b[lenB--] - '0');
+			} else if(lenB < 0) {
+				key = flag + (a[lenA--] - '0');
+			} else {
+				key = flag + (a[lenA--] - '0') + (b[lenB--] - '0');
+			}
+			flag = key > 1 ? 1 : 0;
+			res.push_back(key%2+'0');
+		}
+		if(flag == 1) res.push_back('1');
+		reverse(res.begin(), res.end());
+		return res;
+	}
+};
