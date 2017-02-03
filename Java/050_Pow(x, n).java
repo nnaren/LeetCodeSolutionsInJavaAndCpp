fb0f79a9e6f14ca03833258b2464bceda0fb6721
He Zhang (Rudy) <425220727@qq.com>
2017-02-03 15:48:20 +0800

    Create 050_Pow(x, n).java

---
+++ Java/050_Pow(x, n).java
@@ -0,0 +1,7 @@
+public class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0) return 1;
+        double res = myPow(x, n/2);
+        return n % 2 == 0 ? res*res : (n < 0 ? res*res*(1/x) : res*res*x);
+    }
+}
