1eeb77475526f7f65b1c4e8693fca307f4dea1e3
He Zhang (Rudy) <425220727@qq.com>
2017-02-03 16:13:59 +0800

    Update 050_Pow(x, n).cpp

--- Cpp/050_Pow(x, n).cpp
+++ Cpp/050_Pow(x, n).cpp
@@ -6,3 +6,18 @@ public:
         return n%2 == 0 ? res*res : (n < 0 ? res*res*(1/x) : res*res*x);
     }
 };
+
+//method 2
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == INT_MIN && x > 1) return 0;
+        if(n == 0) return 1;
+        if(n < 0) {
+            n = -n;
+            x = 1/x;
+        }
+        return (n%2 == 0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
+    }
+};
+
