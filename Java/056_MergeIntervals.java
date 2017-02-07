8da67aa17a2fa1c541ecd91b49909a72cfd02e09
He Zhang (Rudy) <425220727@qq.com>
2017-02-07 17:19:45 +0800

    Update 056_MergeIntervals.java

--- Java/056_MergeIntervals.java
+++ Java/056_MergeIntervals.java
@@ -1,4 +1,35 @@
 /**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+   public List<Interval> merge(List<Interval> intervals) {
+		List<Interval> res = new ArrayList<>();
+		if(intervals.isEmpty()) return res;
+		Collections.sort(intervals, new Comparator<Interval>() {
+			public int compare(Interval i1, Interval i2) {
+				return Integer.compare(i1.start, i2.start);
+			}
+		});
+		res.add(intervals.get(0));
+		for(int i = 1; i < intervals.size(); i++) {
+			if(res.get(res.size()-1).end < intervals.get(i).start)
+				res.add(intervals.get(i));
+			else 
+				res.get(res.size()-1).end = Math.max(res.get(res.size()-1).end, intervals.get(i).end);
+		}
+		return res;
+	}
+}
+
+
+// method 2
+/**
  * Definition for an interval. public class Interval { int start; int end;
  * Interval() { start = 0; end = 0; } Interval(int s, int e) { start = s; end =
  * e; } }
