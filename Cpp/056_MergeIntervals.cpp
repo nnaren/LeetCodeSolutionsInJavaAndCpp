da854d308276efee50b5ecc74227b66bf0cf9070
He Zhang (Rudy) <425220727@qq.com>
2017-02-07 17:02:19 +0800

    Update 056_MergeIntervals.cpp

--- Cpp/056_MergeIntervals.cpp
+++ Cpp/056_MergeIntervals.cpp
@@ -1,3 +1,20 @@
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& ins) {
+    if (ins.empty()) return vector<Interval>{};
+    vector<Interval> res;
+    sort(ins.begin(), ins.end(), [](Interval a, Interval b){return a.start < b.start;});
+    res.push_back(ins[0]);
+    for (int i = 1; i < ins.size(); i++) {
+        if (res.back().end < ins[i].start) res.push_back(ins[i]);
+        else
+            res.back().end = max(res.back().end, ins[i].end);
+    }
+    return res;
+}
+};
+
+// method 2
 /**
  * Definition for an interval.
  * struct Interval {
