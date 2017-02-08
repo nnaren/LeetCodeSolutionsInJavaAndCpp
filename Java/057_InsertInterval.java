99fc2eda4e40bffa485128547424818e842d0918
He Zhang (Rudy) <425220727@qq.com>
2017-02-08 14:42:23 +0800

    Create 057_InsertInterval.java

---
+++ Java/057_InsertInterval.java
@@ -0,0 +1,20 @@
+public class Solution {
+	public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+		List<Interval> result = new ArrayList<Interval>();
+		for (Interval i : intervals) {
+			if (newInterval == null || i.end < newInterval.start)
+				result.add(i);
+			else if (i.start > newInterval.end) {
+				result.add(newInterval);
+				result.add(i);
+				newInterval = null;
+			} else {
+				newInterval.start = Math.min(newInterval.start, i.start);
+				newInterval.end = Math.max(newInterval.end, i.end);
+			}
+		}
+		if (newInterval != null)
+			result.add(newInterval);
+		return result;
+	}
+}
