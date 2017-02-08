dc4e574576db1b7e97bade885986bf33e866b81c
He Zhang (Rudy) <425220727@qq.com>
2017-02-08 16:30:42 +0800

    Update 058_LengthOfLastWord.cpp

--- Cpp/058_LengthOfLastWord.cpp
+++ Cpp/058_LengthOfLastWord.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int len = s.length();
+        if(len == 0) return 0;
+        int from = 0, to = len-1;
+		while(from < len && s[from] == ' ' ) from++;
+		while(to > from && s[to] == ' ') to--;
+		return length(s, from, to);
+    }
+    
+    int length(string& s, int from, int to) {
+		int lastBlank = INT_MAX;	
+		for(int i = from, j = 0; i <= to; i++, j++) {
+			if(s[i] == ' ') { 
+				lastBlank = j;
+			}
+		}
+		if(lastBlank == INT_MAX) return to-from+1;
+		return to-from+1-(lastBlank+1);
+	}
+};
 
+	
+	
