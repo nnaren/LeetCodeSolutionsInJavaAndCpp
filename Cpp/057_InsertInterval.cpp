baf8d0b970f9f84a58a713dce166500c37c36bb5
He Zhang (Rudy) <425220727@qq.com>
2017-02-08 15:15:29 +0800

    Update 057_InsertInterval.cpp

--- Cpp/057_InsertInterval.cpp
+++ Cpp/057_InsertInterval.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
+		vector<Interval> res;
+		for (Interval i : intervals) {
+			if (newInterval.start == INT_MAX || i.end < newInterval.start)
+				res.push_back(i);
+			else if (i.start > newInterval.end) {
+				res.push_back(newInterval);
+				res.push_back(i);
+				newInterval.start = INT_MAX;
+			} else {
+				newInterval.start = min(newInterval.start, i.start);
+				newInterval.end = max(newInterval.end, i.end);
+			}
+		}
 
+		if (newInterval.start != INT_MAX)
+			res.push_back(newInterval);
+		return res;
+	}
+};
