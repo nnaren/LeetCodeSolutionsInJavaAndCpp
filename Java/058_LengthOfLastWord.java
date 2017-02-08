f644e8b536c349a1de68cf7fefb2e94bc2605f8d
He Zhang (Rudy) <425220727@qq.com>
2017-02-08 16:26:26 +0800

    Create 058_LengthOfLastWord.java

---
+++ Java/058_LengthOfLastWord.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+		int len = s.length();
+		if(len == 0) return 0;
+		int from = 0, to = len-1;
+		while(from < len && s.charAt(from) == ' ' ) from++;
+		while(to > from && s.charAt(to) == ' ') to--;
+		return length(s, from, to);
+    }
+	
+	private int length(String s, int from, int to) {
+		int lastBlank = Integer.MAX_VALUE;	
+		for(int i = from, j = 0; i <= to; i++, j++) {
+			if(s.charAt(i) == ' ') { 
+				lastBlank = j;
+			}
+		}
+		if(lastBlank == Integer.MAX_VALUE) return to-from+1;
+		return to-from+1-(lastBlank+1);
+	}
+}
