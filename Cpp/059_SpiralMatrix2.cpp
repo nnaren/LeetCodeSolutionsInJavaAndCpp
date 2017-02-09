e4214128931330d4108133e37968f13436c41264
He Zhang (Rudy) <425220727@qq.com>
2017-02-09 14:07:42 +0800

    Update 059_SpiralMatrix2.cpp

--- Cpp/059_SpiralMatrix2.cpp
+++ Cpp/059_SpiralMatrix2.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> res(n, vector<int>(n, 0));
+        if (n != 0) {
+            circleIn(res, 0, n-1, 0, n-1, 1);
+        }
+        return res;
+    }
+    
+    void circleIn(vector<vector<int>>& res, int row_from, int row_to, int col_from, int col_to, int start) {
+		if (row_to < row_from || col_to < col_from) {
+			return;
+		}
+		for (int j = col_from; j <= col_to; ++j) 
+			res[row_from][j] = start++;
+		for (int i = row_from+1; i <= row_to; ++i)
+			res[i][col_to] = start++;
+		for (int j = col_to-1; j >= col_from; --j)
+			res[row_to][j] = start++;
+		for (int i = row_to-1; i >= row_from+1; --i)
+			res[i][col_from] = start++;
+		circleIn(res, row_from+1, row_to-1, col_from+1, col_to-1, start);
+	} 
+};
