635ed0553ff8e18a608e1cd8e96eea6d418ad16c
He Zhang (Rudy) <425220727@qq.com>
2017-02-11 15:14:44 +0800

    Update 062_UniquePaths.java

--- Java/062_UniquePaths.java
+++ Java/062_UniquePaths.java
@@ -1,5 +1,22 @@
 public class Solution {
     public int uniquePaths(int m, int n) {
+        int N = m + n - 2; // how much steps we need to do
+        int K = m - 1;     // number of steps that need to go down
+        double res = 1;
+        // here we calculate the total possible path number 
+        // Combination(N, k) = n! / (k!(n - k)!)
+        // reduce the numerator and denominator and get
+        // C = ( (n - k + 1) * (n - k + 2) * ... * n ) / k!
+        for (int i = 1; i <= K; i++) {
+            res = res * (N - K + i) / i;
+        }
+        return (int)res;
+    }
+}
+/////////////////////////////////////////////////////////////////
+// dp
+public class Solution {
+    public int uniquePaths(int m, int n) {
         if (m == 0 || n == 0) return 0;
         if (m == 1 || n == 1) return 1;
         int[][] dp = new int[m][n];
