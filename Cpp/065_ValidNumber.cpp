10a8e893e9b4494ab9b7dd6d48de95d87b15b5e7
He Zhang (Rudy) <425220727@qq.com>
2017-02-12 16:23:26 +0800

    Update 065_ValidNumber.cpp

--- Cpp/065_ValidNumber.cpp
+++ Cpp/065_ValidNumber.cpp
@@ -10,3 +10,41 @@ At the and it is only valid if there was at least 1 number and if we did see an 
 So basically the number should match this regular expression:
 
 [-+]?(([0-9]+(.[0-9]*)?)|.[0-9]+)(e[-+]?[0-9]+)?
+
+// CODE
+class Solution {
+public:
+    bool isNumber(string s) {
+        bool numberSeen = false;
+		bool pointSeen = false;
+		bool eSeen = false;
+		bool numberAfterE = true;
+
+		int beginIndex = 0, endIndex = s.length()-1;
+		while (s[beginIndex] == ' ') beginIndex++;
+		while (s[endIndex] == ' ') endIndex--;
+
+		for (int i = beginIndex; i <= endIndex; i++) {
+
+			if ('0' <= s[i] && s[i] <= '9') {
+				numberSeen = true;
+				numberAfterE = true;
+			} else if (s[i] == '.') {
+				if (pointSeen || eSeen)
+					return false;
+				pointSeen = true;
+			} else if (s[i] == 'e') {
+				if (eSeen || !numberSeen)
+					return false;
+				eSeen = true;
+				numberAfterE = false;
+			} else if (s[i] == '+' || s[i] == '-') {
+				if (i != beginIndex && s[i-1] != 'e') 
+					return false;
+			} else {
+				return false;
+			}
+		}
+		return numberAfterE && numberSeen;
+    }
+};
