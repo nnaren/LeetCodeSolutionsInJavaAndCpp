fb47c4926b3e85f71b8b7f92d8fdc779a2c9afd8
He Zhang (Rudy) <425220727@qq.com>
2017-02-12 19:02:30 +0800

    Update 069_Sqrt(x).cpp

--- Cpp/069_Sqrt(x).cpp
+++ Cpp/069_Sqrt(x).cpp
@@ -29,3 +29,22 @@ public:
 		return x;
     }
 };
+
+// method 2 binary search
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (0 == x) return 0;
+        int left = 1, right = x, ans;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                left = mid + 1;
+                ans = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
