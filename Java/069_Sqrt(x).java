7b6f6c642ae69b5393e54f9f7b1dd8fc27dc2548
He Zhang (Rudy) <425220727@qq.com>
2017-02-12 19:04:49 +0800

    Update 069_Sqrt(x).java

--- Java/069_Sqrt(x).java
+++ Java/069_Sqrt(x).java
@@ -29,3 +29,22 @@ public class Solution {
         return (int)x;
     }
 }
+
+
+//method 2 binary search
+public class Solution {
+    public int mySqrt(int x) {
+        if (0 == x) return 0;
+        int left = 1, right = x, ans = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                left = mid + 1;
+                ans = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
