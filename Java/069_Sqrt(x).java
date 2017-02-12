b78c192c067e07f6ae506df9eb814b10f248f401
He Zhang (Rudy) <425220727@qq.com>
2017-02-12 18:58:00 +0800

    Update 069_Sqrt(x).java

--- Java/069_Sqrt(x).java
+++ Java/069_Sqrt(x).java
@@ -1,8 +1,8 @@
 public class Solution {
     public int mySqrt(int n) {
         long x = n;
-		    while (x * x > n)
-			    x = (x + n/x) / 2;
-		    return (int)x;
+	while (x * x > n)
+	    x = (x + n/x) / 2;
+        return (int)x;
     }
 }
