03722dcfd869af97839685884327363ca39d452e
He Zhang (Rudy) <425220727@qq.com>
2017-02-13 14:15:01 +0800

    Update 070_ClimbingStairs.cpp

--- Cpp/070_ClimbingStairs.cpp
+++ Cpp/070_ClimbingStairs.cpp
@@ -1,3 +1,16 @@
+// http://www.2cto.com/kf/201509/442774.html
+// 利用离散时间信号系统令输入响应的模型来求解斐波那契问题 f(n) = f(n-1) + f(n-2)
+class Solution {
+public:
+    int climbStairs(int n) {
+        double sqrtNum = sqrt(5);
+    	return (int)(((pow((1 + sqrtNum) / 2, n+1) - pow((1 - sqrtNum) / 2, n+1)) / sqrtNum));
+    }
+};
+
+
+
+// dp
 class Solution {
 public:
     int climbStairs(int n) {
