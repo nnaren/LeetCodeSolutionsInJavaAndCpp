c655e0323eebf3f828bd0bffac9ea596df6ff175
He Zhang (Rudy) <425220727@qq.com>
2017-02-14 15:25:23 +0800

    Update 071_SimplifyPath.cpp

--- Cpp/071_SimplifyPath.cpp
+++ Cpp/071_SimplifyPath.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    string simplifyPath(string path) {
+	    string res("/");
+	    vector<string> stack;
+	    stringstream ss(path);
+	    string temp;
+	    while (getline(ss, temp, '/')) {
+			if (temp == "..") {
+				if (!stack.empty())
+					stack.pop_back();
+			} else if (temp != "" && temp != ".")
+				stack.push_back(temp);
+	    }
+		for (string s : stack)
+			res += (s + "/");
+		if (!stack.empty())
+			res.erase(res.length()-1);
+		return res;
+    }
+};
