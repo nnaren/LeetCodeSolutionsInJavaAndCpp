e2cdb1c4be54d5dedc6750e689f11c2919e715f3
He Zhang (Rudy) <425220727@qq.com>
2017-02-15 14:38:56 +0800

    Create 075_SortColors.cpp

---
+++ Cpp/075_SortColors.cpp
@@ -0,0 +1,36 @@
+// quickSort
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int len = nums.size();
+        if (len <= 1) return ;
+        quickSort(nums, 0, len-1);
+    }
+    
+    int partition (int lo, int hi, vector<int>& nums) {
+    	int key = nums[hi];
+    	int i = lo - 1;
+    	for (int j = lo; j < hi; j++) {
+    		if (nums[j] <= key) {
+    			i++;
+    			swap(i, j, nums);
+    		}
+    	}
+    	swap(i+1, hi, nums);
+    	return i + 1;
+    }
+    
+    void swap(int i, int j, vector<int>& nums) {
+    	int tmp = nums[i];
+    	nums[i] = nums[j];
+    	nums[j] = tmp;
+    }
+    
+    void quickSort(vector<int>& nums, int lo, int hi) {
+    	if (lo < hi) {
+    		int mid = partition(lo, hi, nums);
+    		quickSort(nums, lo, mid-1);
+    		quickSort(nums, mid+1, hi);
+    	}
+    }
+};
