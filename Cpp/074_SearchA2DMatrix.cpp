0647c66ac56e21e6371eeb25fe393ddca1cc07af
He Zhang (Rudy) <425220727@qq.com>
2017-02-15 14:08:59 +0800

    Update 074_SearchA2DMatrix.cpp

--- Cpp/074_SearchA2DMatrix.cpp
+++ Cpp/074_SearchA2DMatrix.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int> > &matrix, int target) {
+        int row = matrix.size();
+        if (row != 0) {
+            int col = matrix[0].size();
+            int begin = 0, end = row * col - 1;
+            while (begin <= end) {
+                int mid = (begin + end) / 2;
+                int mid_value = matrix[mid / col][mid % col];
+                if (mid_value == target)
+                    return true;
+                else if (mid_value < target)
+                    begin = mid + 1;
+                else 
+                    end = mid - 1;
+            }
+        }
+        return false;
+    }
+};
 
