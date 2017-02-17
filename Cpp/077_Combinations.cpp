4162fa99c9290ffc9e0a34d5813286dae5297742
He Zhang (Rudy) <425220727@qq.com>
2017-02-17 17:15:24 +0800

    Update 077_Combinations.cpp

--- Cpp/077_Combinations.cpp
+++ Cpp/077_Combinations.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+	vector<vector<int>> combine(int n, int k) {
+		vector<vector<int>> res;
+		int i = 0;
+		vector<int> temp(k, 0);
+		
+		while (i >= 0) {
+		    temp[i]++;
+		    if (temp[i] > n) --i;
+		    else if (i == k - 1) res.push_back(temp);
+		    else {
+		        ++i;
+		        temp[i] = temp[i-1];
+		    }
+		}
+	    return res;	
+	}
+};
