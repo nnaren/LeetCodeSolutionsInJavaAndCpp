260d0ba3f3968ee5d2fc53efb662c466c29afed8
He Zhang (Rudy) <425220727@qq.com>
2017-02-18 16:26:09 +0800

    Update 078_Subsets.cpp

--- Cpp/078_Subsets.cpp
+++ Cpp/078_Subsets.cpp
@@ -1,3 +1,33 @@
+// Iterative
+This problem can also be solved iteratively. Take [1, 2, 3] in the problem statement as an example.
+The process of generating all the subsets is like:
+
+Initially: \[\[\]\]
+Adding the first number to all the existed subsets: [[], [1]];
+Adding the second number to all the existed subsets: [[], [1], [2], [1, 2]];
+Adding the third number to all the existed subsets: [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]].
+Have you got the idea :-)
+
+The code is as follows.
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subs(1, vector<int>());
+        for (int i = 0; i < nums.size(); i++) {
+            int n = subs.size();
+            for (int j = 0; j < n; j++) {
+                subs.push_back(subs[j]); 
+                subs.back().push_back(nums[i]);
+            }
+        }
+        return subs;
+    }
+}; 
+    
+
+
+// backTrack
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
