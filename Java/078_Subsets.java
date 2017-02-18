67c8645e83a2078efd93f9a46ed0c05a6203ff53
He Zhang (Rudy) <425220727@qq.com>
2017-02-18 16:23:53 +0800

    Update 078_Subsets.java

--- Java/078_Subsets.java
+++ Java/078_Subsets.java
@@ -1,3 +1,34 @@
+// Iterative
+This problem can also be solved iteratively. Take [1, 2, 3] in the problem statement as an example.
+The process of generating all the subsets is like:
+
+Initially: \[\[\]\]
+Adding the first number to all the existed subsets: [[], [1]];
+Adding the second number to all the existed subsets: [[], [1], [2], [1, 2]];
+Adding the third number to all the existed subsets: [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]].
+Have you got the idea :-)
+
+The code is as follows.
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+		List<List<Integer>> subs = new ArrayList<>();
+		subs.add(new ArrayList<>());
+		
+		for (int i = 0; i < nums.length; ++i) {
+			int n = subs.size();
+			for (int j = 0; j < n; j++) {
+				List<Integer> temp = new ArrayList<>();
+				for (int e : subs.get(j))
+					temp.add(e);
+				subs.add(temp);
+				subs.get(subs.size()-1).add(nums[i]);
+			}
+		}
+		return subs;
+    }
+}
+
+// backTrack
 public List<List<Integer>> subsets(int[] nums) {
 		Arrays.sort(nums);
 		List<List<Integer>> subs = new ArrayList<>();
