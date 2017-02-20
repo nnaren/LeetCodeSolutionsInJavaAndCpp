d5cc13a632d4f731a55172bccb28352a163650e7
He Zhang (Rudy) <425220727@qq.com>
2017-02-20 17:19:42 +0800

    Update 088_MergeSortedArray.java

--- Java/088_MergeSortedArray.java
+++ Java/088_MergeSortedArray.java
@@ -1,3 +1,16 @@
+// best
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, tar = m + n - 1;
+        while (j >= 0)
+        	if (i >= 0)
+        		nums1[tar--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+        	else
+        		nums1[tar--] = nums2[j--];
+    }
+}
+
+// bad
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int[] aux = new int[m+n];
