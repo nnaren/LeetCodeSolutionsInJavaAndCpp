74d83ae85c80b34972583ffcaf907b7e5126bb81
He Zhang (Rudy) <425220727@qq.com>
2017-02-20 14:56:34 +0800

    Create 081_SearchInRotatedSortedArray2.cpp

---
+++ Cpp/081_SearchInRotatedSortedArray2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+    	int lo = 0, hi = nums.size() - 1;
+    	while (lo <= hi) {
+    		int mid = (lo + hi) / 2;
+    		if (nums[mid] == target) return true;
+    		else if (nums[lo] < nums[mid]) {
+    			if (nums[lo] <= target && target < nums[mid]) hi = mid - 1;
+    			else lo = mid + 1;
+    		} else if (nums[lo] > nums[mid]) {
+    			if (nums[mid] < target && target <= nums[hi]) lo = mid + 1;
+    			else hi = mid - 1;
+    		} else lo++;
+    	}
+    	return false;
+    }
+};
