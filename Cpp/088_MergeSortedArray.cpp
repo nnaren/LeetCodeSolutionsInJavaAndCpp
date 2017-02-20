9300149ceb85ebb075c482ed680a853094e914af
He Zhang (Rudy) <425220727@qq.com>
2017-02-20 17:21:16 +0800

    Update 088_MergeSortedArray.cpp

--- Cpp/088_MergeSortedArray.cpp
+++ Cpp/088_MergeSortedArray.cpp
@@ -1,3 +1,19 @@
+// best
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1, j = n - 1, tar = m + n - 1;
+        while (j >= 0) {
+            if (i >= 0)
+        		nums1[tar--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+        	else
+        		nums1[tar--] = nums2[j--];
+        }
+    }
+};
+
+
+// bad
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
