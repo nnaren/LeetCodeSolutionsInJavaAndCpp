854686dae243128da62c7efa5da7a7068e03a5c6
He Zhang (Rudy) <425220727@qq.com>
2017-02-20 16:55:30 +0800

    Create 088_MergeSortedArray.java

---
+++ Java/088_MergeSortedArray.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] aux = new int[m+n];
+        int i = 0, j = m;
+        for (int k = 0; k < m; k++) {
+        	aux[k] = nums1[k];
+        }
+        for (int k = 0; k < n; k++) {
+        	aux[m + k] = nums2[k];
+        }
+        
+        for (int k = 0; k < m+n; k++) {
+        	if (i >= m) nums1[k] = aux[j++];
+        	else if (j >= n+m) nums1[k] = aux[i++];
+        	else if (aux[i] < aux[j]) nums1[k] = aux[i++];
+        	else nums1[k] = aux[j++];
+        }
+    }
+}
