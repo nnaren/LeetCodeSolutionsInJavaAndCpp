2b7c3e52369685dd0119de9fd86eab8a2b45c6a0
He Zhang (Rudy) <425220727@qq.com>
2017-02-20 16:11:54 +0800

    Update 082_RemoveDuplicatesFromSortedList2.cpp

--- Cpp/082_RemoveDuplicatesFromSortedList2.cpp
+++ Cpp/082_RemoveDuplicatesFromSortedList2.cpp
@@ -1,1 +1,21 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == nullptr) return head;
+        if (head->next != nullptr && head->val == head->next->val) {
+            while (head->next != nullptr && head->val == head->next->val) 
+                head = head->next;
+            return deleteDuplicates(head->next);
+        } else 
+            head->next = deleteDuplicates(head->next);
+        return head;
+    }
+};
