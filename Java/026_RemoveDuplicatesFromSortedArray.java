4a453e9437e399ead1497c09fa15dd357413d4e3
He Zhang (Rudy) <425220727@qq.com>
2017-02-20 14:14:17 +0800

    Update 026_RemoveDuplicatesFromSortedArray.java

--- Java/026_RemoveDuplicatesFromSortedArray.java
+++ Java/026_RemoveDuplicatesFromSortedArray.java
@@ -22,6 +22,16 @@
 //	}
 //}
 
+// best
+public int removeDuplicates(int[] nums) {
+    int i = 0;
+    for(int n : nums)
+        if(i < 1 || n > nums[i - 1]) 
+            nums[i++] = n;
+    return i;
+}
+
+// another method
 package leetCode;
 
 public class RemoveDuplicatesfromSortedArray {
