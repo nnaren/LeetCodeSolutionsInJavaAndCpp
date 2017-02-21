e7f9918c07b7def1f0d36107088e14905c7693c6
He Zhang (Rudy) <425220727@qq.com>
2017-02-21 14:57:14 +0800

    Update 084_LargestRectangleInHistogram.cpp

--- Cpp/084_LargestRectangleInHistogram.cpp
+++ Cpp/084_LargestRectangleInHistogram.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& height) {
+        int n = height.size(), area = 0;
+        stack<int> indexes;
+        
+        for (int i = 0; i <= n; i++) {
+            int height_bound = (i == height.size()) ? 0 : height[i];
+            while (!indexes.empty()) {
+                int h = height[indexes.top()]; 
+                if (h < height_bound) break;
+                indexes.pop();
+                int index_bound = indexes.empty() ? -1 : indexes.top();
+                area = max(area, h * (i - index_bound - 1));
+            }
+            indexes.push(i);
+        }
+        return area; 
+    }
+};
