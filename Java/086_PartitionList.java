2344c4810f0d048e8c19151f8a5bf6a9e1e01fe2
He Zhang (Rudy) <425220727@qq.com>
2017-02-23 15:35:07 +0800

    Update 086_PartitionList.java

--- Java/086_PartitionList.java
+++ Java/086_PartitionList.java
@@ -1,3 +1,32 @@
+// Good
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode node1 = new ListNode(0);
+        ListNode node2 = new ListNode(0);
+        ListNode p1 = node1, p2 = node2;
+        while (head != null) {
+            if (head.val < x)
+                p1 = p1.next = head;
+            else
+                p2 = p2.next = head;
+            head = head.next;
+        }
+        p2.next = null;
+        p1.next = node2.next;
+        return node1.next;
+    }
+}
+
+
+// Bad
 /**
  * Definition for singly-linked list.
  * public class ListNode {
