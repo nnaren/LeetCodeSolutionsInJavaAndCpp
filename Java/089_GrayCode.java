d28f0a8a606ee40a49621c1233d500471d4e5872
He Zhang (Rudy) <425220727@qq.com>
2017-02-23 17:37:08 +0800

    Update 089_GrayCode.java

--- Java/089_GrayCode.java
+++ Java/089_GrayCode.java
@@ -6,3 +6,21 @@ public class Solution {
         return result;
     }
 }
+
+// rescursive
+public class Solution {
+	public List<Integer> grayCode(int n) {
+	    List<Integer> res = new LinkedList<>();
+	    res.add(0);
+	    for (int i = 0; i < n; i++) {
+	    	int curCount = res.size();
+	    	while (curCount-- > 0) {
+	    		int curNum = res.get(curCount);
+	    		curNum += (1 << i);
+	    		res.add(curNum);
+	    	}
+	    }
+	    return res;
+	}
+}
+
