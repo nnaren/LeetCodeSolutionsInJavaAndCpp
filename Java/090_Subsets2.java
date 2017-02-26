0213d95f67b844bf6d9e6768053d5d5546eb5ea6
He Zhang (Rudy) <425220727@qq.com>
2017-02-26 14:36:16 +0800

    Update 090_Subsets2.java

--- Java/090_Subsets2.java
+++ Java/090_Subsets2.java
@@ -1,3 +1,28 @@
+// bacttrack
+public class Solution {
+	public List<List<Integer>> subsetsWithDup(int[] nums) {
+		Arrays.sort(nums);
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		List<Integer> temp = new ArrayList<>();
+		subsetsWithDup(res, nums, temp, 0);
+	    return res;
+	}
+	  
+	private void subsetsWithDup(List<List<Integer>> res, int[] nums, List<Integer> temp, int begin) {
+		List<Integer> sub = new ArrayList<>();
+		for (int e : temp) sub.add(e);
+		res.add(sub);
+		for (int i = begin; i < nums.length; ++i) {
+			if (i == begin || nums[i] != nums[i-1]) {
+				temp.add(nums[i]);
+				subsetsWithDup(res, nums, temp, i+1);
+				temp.remove(temp.size()-1);
+		    }	  
+	    }
+	}
+}
+
+// iterative
 public class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
     	Arrays.sort(nums);
