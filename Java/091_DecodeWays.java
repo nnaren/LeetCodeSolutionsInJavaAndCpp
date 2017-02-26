ca1744b53ede6d2455377e7d16a31abdacc09fdc
He Zhang (Rudy) <425220727@qq.com>
2017-02-26 17:09:14 +0800

    Update 091_DecodeWays.java

--- Java/091_DecodeWays.java
+++ Java/091_DecodeWays.java
@@ -1,3 +1,35 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+    	int len = s.size();
+    	if (len == 0 || s[0] == '0')
+    		return 0;
+    	if (len == 1) return 1;
+    	int left = 1, right = 1, num = 0;
+    	for (int i = 1; i < len; i++) {
+    		num = 0;
+    		if (!isValid(s[i]) && !isValid(s[i-1], s[i]))
+    			return 0;
+    		if (isValid(s[i])) 
+    			num = right;
+    		if (isValid(s[i-1], s[i]))
+    			num += left;
+    		left = right;
+    		right = num;
+    	}
+    	return num;
+    }
+    
+    bool isValid(char a, char b) {
+    	return a == '1' || (a == '2' && b <= '6');
+    }
+    
+    bool isValid(char a) {
+    	return a != '0';
+    }
+};
+
+// method 2
 public class Solution {
     public int numDecodings(String s) {
     	int len = s.length();
