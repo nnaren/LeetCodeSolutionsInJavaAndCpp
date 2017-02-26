c8c64a3fbcb3fa7dc76821da22d6f19c5fb64e86
He Zhang (Rudy) <425220727@qq.com>
2017-02-26 14:09:47 +0800

    Update 090_Subsets2.cpp

--- Cpp/090_Subsets2.cpp
+++ Cpp/090_Subsets2.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    vector<vector<int> > subsetsWithDup(vector<int> &S) {
+        sort(S.begin(), S.end());
+        vector<vector<int>> ret = {{}};
+        int size = 0, startIndex = 0;
+        for (int i = 0; i < S.size(); i++) {
+            startIndex = i >= 1 && S[i] == S[i - 1] ? size : 0;
+            size = ret.size();
+            for (int j = startIndex; j < size; j++) {
+                vector<int> temp = ret[j];
+                temp.push_back(S[i]);
+                ret.push_back(temp);
+            }
+        }
+        return ret;
+    }
+};
