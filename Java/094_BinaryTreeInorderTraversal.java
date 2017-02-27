8b5dd5dbf5252c62bd714c31d078917a7f35ee6f
He Zhang (Rudy) <425220727@qq.com>
2017-02-27 17:05:33 +0800

    Update 094_BinaryTreeInorderTraversal.java

--- Java/094_BinaryTreeInorderTraversal.java
+++ Java/094_BinaryTreeInorderTraversal.java
@@ -1,3 +1,32 @@
+// Iterative
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> s = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !s.empty()) {
+            while (cur != null) {
+                s.add(cur);
+                cur = cur.left;
+            }
+            cur = s.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
+        return res;
+    }
+}
+
+// Recursion
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
