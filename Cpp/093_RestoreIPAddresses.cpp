e68675f6c2bc5f086c7defc8edf0b8e0594be797
He Zhang (Rudy) <425220727@qq.com>
2017-02-27 16:19:16 +0800

    Update 093_RestoreIPAddresses.cpp

--- Cpp/093_RestoreIPAddresses.cpp
+++ Cpp/093_RestoreIPAddresses.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> res;
+        string ans;
+        int len = s.size();
+        for (int a=1; a<=3; a++)
+        for (int b=1; b<=3; b++)
+        for (int c=1; c<=3; c++) {
+            int d = len - a - b - c;
+            if (d > 0 && d <= 3 && a+b+c+d == s.size()) {
+                int A = stoi(s.substr(0, a));
+                int B = stoi(s.substr(a, b));
+                int C = stoi(s.substr(a+b, c));
+                int D = stoi(s.substr(a+b+c, d));
+                if (A > 255 || B > 255 || C > 255 || D > 255) continue;
+                ans = to_string(A) + "." + to_string(B) + "." + to_string(C) + "." + to_string(D);
+                if (ans.size() == len + 3)
+                    res.push_back(ans);
+            }  
+        }
+        return res;
+    }
+};
