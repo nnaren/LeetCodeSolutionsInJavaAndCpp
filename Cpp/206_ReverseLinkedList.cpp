d209c9c04310856a18032f87ac3b9334e070c271
He Zhang (Rudy) <425220727@qq.com>
2017-02-27 14:52:29 +0800

    Update 206_ReverseLinkedList.cpp

--- Cpp/206_ReverseLinkedList.cpp
+++ Cpp/206_ReverseLinkedList.cpp
@@ -1,1 +1,25 @@
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr)
+            return head;
+        ListNode* p1 = head;
+        ListNode* p2 = p1->next;
+        while (p2 != nullptr) {
+            ListNode * p3 = p2->next;
+            p2->next = p1;
+            p1 = p2;
+            p2 = p3;
+        }
+        head->next = nullptr;
+        return p1;
+    }
+};
