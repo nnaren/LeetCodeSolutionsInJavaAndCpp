931364c8954c40caf78f9336206e57f1ca248b7d
He Zhang (Rudy) <425220727@qq.com>
2017-02-27 14:47:50 +0800

    Update 206_ReverseLinkedList.java

--- Java/206_ReverseLinkedList.java
+++ Java/206_ReverseLinkedList.java
@@ -1,3 +1,30 @@
+// 反向
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode p1 = head;
+        ListNode p2 = p1.next;
+        while (p2 != null) {
+            ListNode p3 = p2.next;
+            p2.next = p1;
+            p1 = p2;
+            p2 = p3;
+        }
+        head.next = null;
+        return p1;
+    }
+}
+
+// 正向
 /**
  * Definition for singly-linked list.
  * public class ListNode {
