6280668bf9bbdfcf50a20511dd30c87a3d165b4f
He Zhang (Rudy) <425220727@qq.com>
2017-02-28 17:44:38 +0800

    Update 094_BinaryTreeInorderTraversal.java

--- Java/094_BinaryTreeInorderTraversal.java
+++ Java/094_BinaryTreeInorderTraversal.java
@@ -1,3 +1,40 @@
+// morris algorithm
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        TreeNode cur = root;
+        List<Integer> res = new ArrayList<>();
+        while (cur != null) {
+            if (cur.left != null) {
+                TreeNode predecessor = cur.left;
+                while (predecessor.right != null && predecessor.right != cur)
+                    predecessor = predecessor.right;
+                if (predecessor.right == null) {
+                    predecessor.right = cur;
+                    cur = cur.left;
+                } else {
+                    predecessor.right = null;
+                    res.add(cur.val);
+                    cur = cur.right;
+                }
+            } else {
+                res.add(cur.val);
+                cur = cur.right;
+            }
+        }
+        return res;
+    }
+}
+
+
 // Iterative
 /**
  * Definition for a binary tree node.
