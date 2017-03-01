73397b37b8cfdf0b9cb37ffeeb57156e4c181fcf
He Zhang (Rudy) <425220727@qq.com>
2017-03-01 14:46:10 +0800

    Update 100_SameTree.java

--- Java/100_SameTree.java
+++ Java/100_SameTree.java
@@ -10,10 +10,9 @@
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null) return true;
-        if (p != null && q != null)
-            if(p.val == q.val)
-                return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-            else return false;
+        if(p == null || q == null) return false;
+        if(p.val == q.val)
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         return false;
     }
 }
