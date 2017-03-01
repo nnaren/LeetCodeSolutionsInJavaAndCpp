dd321307b64c800aeb3fccfe367ad5e709a09f01
He Zhang (Rudy) <425220727@qq.com>
2017-03-01 14:43:48 +0800

    Update 100_SameTree.cpp

--- Cpp/100_SameTree.cpp
+++ Cpp/100_SameTree.cpp
@@ -1,1 +1,20 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == nullptr && q == nullptr) return true;
+        if (p != nullptr && q != nullptr)
+            if(p->val == q->val)
+                return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+            else return false;
+        return false;
+    }
+};
