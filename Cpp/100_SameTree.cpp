b89204f81626527cab743a8a68a6622d79fd086b
He Zhang (Rudy) <425220727@qq.com>
2017-03-01 14:49:50 +0800

    Update 100_SameTree.cpp

--- Cpp/100_SameTree.cpp
+++ Cpp/100_SameTree.cpp
@@ -11,10 +11,9 @@ class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p == nullptr && q == nullptr) return true;
-        if (p != nullptr && q != nullptr)
-            if(p->val == q->val)
-                return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-            else return false;
+        if(p == nullptr || q == nullptr) return false;
+        if(p->val == q->val)
+            return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
         return false;
     }
 };
