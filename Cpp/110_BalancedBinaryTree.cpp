2f9b29ff57d92d36c4c77aa53144dfeb3d12deb8
He Zhang (Rudy) <425220727@qq.com>
2017-03-10 16:19:01 +0800

    Update 110_BalancedBinaryTree.cpp

--- Cpp/110_BalancedBinaryTree.cpp
+++ Cpp/110_BalancedBinaryTree.cpp
@@ -1,1 +1,27 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    bool isBalance = true;
+public:
+    bool isBalanced(TreeNode* root) {
+        getDepth(root);
+        return isBalance;
+    }
+    
+    int getDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int left = getDepth(root->left);
+        int right = getDepth(root->right);
+        if (abs(left - right) > 1)
+            isBalance = false;
+        return right > left ? right+1 : left+1;
+    }
+};
