1e9c32bdd9a328a78faf27f5ec83f5b37b9b5836
He Zhang (Rudy) <425220727@qq.com>
2017-03-16 16:31:12 +0800

    Update 239_SlidingWindowMaximum.cpp

--- Cpp/239_SlidingWindowMaximum.cpp
+++ Cpp/239_SlidingWindowMaximum.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+    vector<int> maxSlidingWindow(vector<int> &A, int B) {
+        vector<int> ans;
+        deque<int> dq;
+        for(int i = 0; i < A.size(); i++){
+            while(!dq.empty() && dq.back() < A[i]) dq.pop_back();
+            dq.push_back(A[i]);
+            if(i >= B - 1){
+                ans.push_back(dq.front());
+                if(A[i - B + 1] == dq.front()) dq.pop_front();
+            }
+        }
+        return ans;
+    }
+};
+
+
+class Solution {
+public:
     vector<int> maxSlidingWindow(vector<int> &nums, int k) {
         int len = nums.size();
         if (len == 0) return nums;
