b4a9414404f4c208244998394fbe0220ccf114a5
He Zhang (Rudy) <425220727@qq.com>
2017-03-16 16:27:16 +0800

    Update 239_SlidingWindowMaximum.java

--- Java/239_SlidingWindowMaximum.java
+++ Java/239_SlidingWindowMaximum.java
@@ -6,6 +6,30 @@ public class Solution {
         
         Deque<Integer> dq = new LinkedList<Integer>();
         for (int i = 0; i < len; i++) {
+        	while (!dq.isEmpty() && dq.peekLast() < nums[i])
+        		dq.pollLast();
+        	
+        	dq.offer(nums[i]);
+        	
+        	if (i >= k - 1) {
+        		res[i - k + 1] = dq.peek();
+        		if (nums[i - k + 1] == dq.peek())
+        			dq.pollFirst();
+        	}
+        }
+    	return res;
+    }
+}
+
+
+public class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int len = nums.length;
+        if (len == 0) return nums;
+        int[] res = new int[len - k + 1];
+        
+        Deque<Integer> dq = new LinkedList<Integer>();
+        for (int i = 0; i < len; i++) {
         	if (!dq.isEmpty() && dq.peek() < i - k + 1)
         		dq.poll();
         	
