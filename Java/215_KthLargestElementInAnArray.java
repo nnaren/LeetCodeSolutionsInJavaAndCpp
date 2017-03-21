a489eb760508d1ecbfdd285bfbef12562e724766
He Zhang (Rudy) <425220727@qq.com>
2017-03-21 16:22:36 +0800

    Update 215_KthLargestElementInAnArray.java

--- Java/215_KthLargestElementInAnArray.java
+++ Java/215_KthLargestElementInAnArray.java
@@ -1,3 +1,62 @@
+// method 1
+// priorityQueue by MinHeap
+// O(N lg K)
+public class Solution {
+	private int parent(int i) {
+		return (i - 1) / 2;
+	}
+	
+	private int leftChild(int i) {
+		return i * 2 + 1;
+	}
+	
+	private int rightChild(int i) {
+		return i * 2 + 2;
+	}
+	
+	private void swap(int[] nums, int i, int j) {
+		int temp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = temp;
+	}
+	
+	private void minHeap(int[] nums, int len, int i) {
+		int lChild = leftChild(i);
+		int rChild = rightChild(i);
+		int minPos = i;
+		if (lChild < len && nums[lChild] < nums[minPos]) minPos = lChild;
+		if (rChild < len && nums[rChild] < nums[minPos]) minPos = rChild;
+		
+		if (minPos != i) {
+			
+			swap(nums, minPos, i);
+			minHeap(nums, len, minPos);
+		}	
+	}
+	
+	private void buildMinHeap(int[] nums, int len) {
+		for	(int i = parent(len - 1); i >= 0; i--)
+			minHeap(nums, len, i);
+	}
+	
+	public int findKthLargest(int[] nums, int k) {
+		if (nums == null || nums.length < k || k <= 0)
+			return -1;
+		int[] minHeap = new int[k];
+		for(int i = 0; i < k; i++) 
+			minHeap[i] = nums[i];
+		buildMinHeap(minHeap, k);
+		
+		for (int i = k; i < nums.length; i++) {
+			if (minHeap[0] < nums[i]) {
+				minHeap[0] = nums[i];
+				minHeap(minHeap, k, 0);
+			}
+		}
+		return minHeap[0];
+	}
+}
+
 // method 2
 //O(N lg K) running time + O(K) memory
 public class Solution {
